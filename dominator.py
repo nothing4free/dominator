fb264fec0e42664c24c1ef3d9477ea313855a231
nothing4free <jorge@nothing4free>
2022-12-14 00:56:00 +0100

    its alive (??)

--- dominator.py
+++ dominator.py
@@ -1,1 +1,138 @@
-# terminal CLI prompt will go in here
+"""    
+    dns_records (main table, scan results go here)
+    +---------+-----------+--------+---+------+-------+-----+----+----+----+------+-----+----------+
+    | scan_id | scan_date | target | a | aaaa | cname | mx | ns | txt | soa | ptr | srv | redirect |
+    +---------+-----------+--------+---+------+-------+-----+----+----+----+------+-----+----------+
+    |   1     | YYYY-MM-DD| domain | a | aaaa | cname | mx | ns | txt | soa | ptr | srv | url / no |
+    +---------+-----------+--------+---+------+-------+-----+----+----+----+------+-----+----------+
+
+    targets (scan sources go here)
+    +---------+
+    | target  |
+    +---------+
+    | domain  |
+    +---------+
+"""
+
+from simple_term_menu import TerminalMenu
+import sqlite3
+import os
+
+def new_screen():
+    os.system("clear")
+    print("        __                _             __            ")
+    print("   ____/ /___  ____ ___  (_)___  ____ _/ /_____  _____")
+    print("  / __  / __ \/ __ `__ \/ / __ \/ __ `/ __/ __ \/ ___/")
+    print(" / /_/ / /_/ / / / / / / / / / / /_/ / /_/ /_/ / /    ")
+    print(" \__,_/\____/_/ /_/ /_/_/_/ /_/\__,_/\__/\____/_/     [dev]")
+    print(" ──────────────────────────────────────────────────────────")
+    print("")
+
+
+def add_target():
+
+    new_screen()
+    try:
+        conn = sqlite3.connect('./databases/intel.db')
+        c = conn.cursor()
+        target = input(" [i] Enter the target domain: ")
+        c.execute("INSERT INTO targets VALUES (?)", (target,))
+        conn.commit()
+        conn.close()
+        print(" [i] Target added successfully")
+    except:
+        print(" [!] Error adding target")
+
+def remove_target():
+    new_screen()
+    try:
+        conn = sqlite3.connect('./databases/intel.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM targets")
+        targets = c.fetchall()
+
+        target_list = []
+        for target in targets:
+            target_list.append(target[0])
+        menu = TerminalMenu(target_list, title=" [i] Select a target to remove")
+        menu_entry_index = menu.show()
+
+        c.execute("DELETE FROM targets WHERE target = ?", (target_list[menu_entry_index],))
+        conn.commit()
+
+        options = ["Yes", "No"]
+        menu = TerminalMenu(options, title=" [i] Remove all records on history for this target?")
+        menu_entry_index = menu.show()
+
+        if menu_entry_index == 0:
+            c.execute("DELETE FROM dns_records WHERE target = ?", (target_list[menu_entry_index],))
+            conn.commit()
+            print(" [i] All records removed successfully")
+        else:
+            print(" [i] Records not removed")
+
+        conn.close()
+    
+    except:
+        print(" [!] Error removing target")
+
+def list_targets_and_records():
+    new_screen()
+    try:
+        conn = sqlite3.connect('./databases/intel.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM targets")
+        targets = c.fetchall()
+
+        target_list = []
+        for target in targets:
+            target_list.append(target[0])
+        menu = TerminalMenu(target_list, title=" [i] Select a target to view its historical records")
+        menu_entry_index = menu.show()
+
+        c.execute("SELECT * FROM dns_records WHERE target = ?", (target_list[menu_entry_index],))
+        records = c.fetchall()
+
+        print(" [i] Records for target: " + target_list[menu_entry_index])
+        print("───────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        print("")
+
+        for record in records:
+            print(" [i] Scan ID: " + str(record[0]))
+            print(" [i] Scan Date: " + str(record[1]))
+            print(" [i] Target: " + str(record[2]))
+            print(" [i] A: " + str(record[3]))
+            print(" [i] AAAA: " + str(record[4]))
+            print(" [i] CNAME: " + str(record[5]))
+            print(" [i] MX: " + str(record[6]))
+            print(" [i] NS: " + str(record[7]))
+            print(" [i] TXT: " + str(record[8]))
+            print(" [i] SOA: " + str(record[9]))
+            print(" [i] PTR: " + str(record[10]))
+            print(" [i] SRV: " + str(record[11]))
+            print(" [i] Redirect: " + str(record[12]))
+            print("───────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+            print("")
+
+        conn.close()
+    except:
+        print(" [!] Error listing targets and records")
+
+def main():
+    new_screen()
+    options = ["Add Target", "Remove Target", "List Targets and Records", "Exit"]
+    menu = TerminalMenu(options, title=" [i] Select an option")
+    menu_entry_index = menu.show()
+
+    if menu_entry_index == 0:
+        add_target()
+    elif menu_entry_index == 1:
+        remove_target()
+    elif menu_entry_index == 2:
+        list_targets_and_records()
+    elif menu_entry_index == 3:
+        exit()
+
+if __name__ == "__main__":
+    while True:
+        main()
--- engine.py
+++ engine.py
@@ -2,6 +2,9 @@ import sqlite3
 import dns
 import dns.resolver
 import sys
+import datetime
+import requests
+import traceback
 
 """
     This script will resolve a hostname to an IP address and print it to the screen.
@@ -16,8 +19,6 @@ import sys
 
     Every day it will check if anything at all changes, and if so, it will save the new data and notify via email.
     Also it will send a daily report with the results.
-
-
 
 
     DATABASE STRUCTURE: 
@@ -39,23 +40,156 @@ import sys
 """ 
 
 
-try:
-    # set the hostname to the argument passed on the script call
-    hostname = sys.argv[1]
-except:
-    # if no argument is passed, set the hostname to the local hostname
-    print(" [!] No hostname specified, please call the script with a hostname as argument")
-    exit()
+def check_for_redirects(hostname):
+    try:
+        r = requests.get("http://" + hostname, allow_redirects=False)
+        if r.status_code == 200:
+            return "no"
+        else:
+            # return the whole URL
+            return r.headers["Location"]
+    except:
+        return "no"
 
 
 def get_dns_records(hostname):
-    a_records = dns.resolver.query(hostname, 'A')
-    aaaa_records = dns.resolver.query(hostname, 'AAAA')
-    cname_records = dns.resolver.query(hostname, 'CNAME')
-    mx_records = dns.resolver.query(hostname, 'MX')
-    ns_records = dns.resolver.query(hostname, 'NS')
-    txt_records = dns.resolver.query(hostname, 'TXT')
-    soa_records = dns.resolver.query(hostname, 'SOA')
-    ptr_records = dns.resolver.query(hostname, 'PTR')
-    srv_records = dns.resolver.query(hostname, 'SRV')
+    scan_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    try:
+        a_result = dns.resolver.resolve(hostname, 'A')
+        a_records = []
+        for rdata in a_result:
+            a_records.append(rdata.address)
+    except:
+        a_records = ""
     
+    try:
+        aaaa_result= dns.resolver.resolve(hostname, 'AAAA')
+        aaaa_records = []
+        for rdata in aaaa_result:
+            aaaa_records.append(rdata.address)
+    except:
+        aaaa_records = ""
+    
+    try:
+        cname_result = dns.resolver.resolve(hostname, 'CNAME')
+        cname_records = []
+        for rdata in cname_result:
+            cname_records.append(rdata.target.to_text())
+    except:
+        # print stack trace
+        #traceback.print_exc()
+        cname_records = ""
+    
+    try:
+        mx_result = dns.resolver.resolve(hostname, 'MX')
+        mx_records = []
+        for rdata in mx_result:
+            mx_records.append(rdata.exchange.to_text())
+    except:
+        mx_records = ""
+    
+    try:
+        ns_result = dns.resolver.resolve(hostname, 'NS')
+        ns_records = []
+        for rdata in ns_result:
+            ns_records.append(rdata.target.to_text())
+    except:
+        ns_records = ""
+    
+    try:
+        txt_result = dns.resolver.resolve(hostname, 'TXT')
+        txt_records_pre = []
+        for rdata in txt_result:
+            txt_records_pre.append(rdata.strings)
+        txt_records = []
+        # remove the b' from the strings in the tuple
+        for x in txt_records_pre:
+            for y in x:
+                y = y.decode("utf-8")
+                txt_records.append(y)
+
+    except:
+        txt_records = ""
+    
+    try:
+        soa_result = dns.resolver.resolve(hostname, 'SOA')
+        soa_records = []
+        for x in soa_result:
+            soa_records.append(x.to_text())
+    except:
+        # print stack trace
+        traceback.print_exc()
+        soa_records = ""
+    
+    try:
+        ptr_result = dns.resolver.resolve(hostname, 'PTR')
+        ptr_records = []
+        for rdata in ptr_result:
+            ptr_records.append(rdata.target.to_text())
+    except:
+        ptr_records = ""
+    
+    try:
+        srv_result = dns.resolver.resolve(hostname, 'SRV')
+        srv_records = []
+        for rdata in srv_result:
+            srv_records.append(rdata.target)
+    except:
+        srv_records = ""
+
+    redirect = check_for_redirects(hostname)
+
+    """conn = sqlite3.connect('./databases/intel.db')
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO dns_records(scan_date, target, a, aaaa, cname, mx, ns, txt, soa, ptr, srv, redirect) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", 
+        (scan_date, 
+        hostname, 
+        a_records, 
+        aaaa_records, 
+        cname_records, 
+        mx_records, 
+        ns_records, 
+        txt_records, 
+        soa_records, 
+        ptr_records, 
+        srv_records, 
+        redirect)
+    )
+    
+    conn.commit()
+    conn.close()"""
+
+    print(" [+] Scan date: " + scan_date)
+    print(" [+] Target: " + hostname)
+    print(" [+] A records: " + str(a_records))
+    print(" [+] AAAA records: " + str(aaaa_records))
+    print(" [+] CNAME records: " + str(cname_records))
+    print(" [+] MX records: " + str(mx_records))
+    print(" [+] NS records: " + str(ns_records))
+    print(" [+] TXT records: " + str(txt_records))
+    print(" [+] SOA records: " + str(soa_records))
+    print(" [+] PTR records: " + str(ptr_records))
+    print(" [+] SRV records: " + str(srv_records))
+    print(" [+] Redirect: " + redirect)
+    print(       )
+    print(str(txt_records)) # a way to save this thing
+
+
+
+def main():
+    try:
+        # set the hostname to the argument passed on the script call
+        hostname = sys.argv[1]
+        get_dns_records(hostname)
+    except Exception as e:
+        # if no argument is passed, set the hostname to the local hostname
+        # print error traceback
+        traceback.print_exc()
+        print(" [!] Error: " + str(e))
+        # print(" [!] No hostname specified, please call the script with a hostname as argument")
+        exit()
+    
+if __name__ == "__main__":
+    main()
